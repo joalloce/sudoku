4c5e5110445175b76c652a2b0d28321038d9a86f
Jose Lo <joalloce@gmail.com>
2023-05-26 22:16:29 -0600

    fix(*): router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { HashRouter, Switch, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import About from "./components/About";
 function App() {
   return (
-    <HashRouter basename="/">
+    <>
       <Header />
-      <Switch>
-        <Route exact path="/" component={Home}></Route>
-        <Route exact path="/about" component={About}></Route>
-      </Switch>
-    </HashRouter>
+      <Routes>
+        <Route path="/" component={Home}></Route>
+        <Route path="/about" component={About}></Route>
+      </Routes>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.Fragment>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.Fragment>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
